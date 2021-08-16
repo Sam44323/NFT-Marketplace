eaad8f8a980f27855ead8393ad7148fcce875ea2
Suranjan <suranjanbiswas2002@gmail.com>
2021-08-17 00:36:58 +0530

    add: added keys to hardhat configs

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,6 +2,7 @@ require("@nomiclabs/hardhat-waffle");
 require("dotenv").config("./env");
 
 const projectId = process.env.local.NEXT_PUBLIC_PROJECT_ID;
+const key = process.env.PRIVATE_KEY;
 
 module.exports = {
   networks: {
@@ -10,6 +11,7 @@ module.exports = {
     },
     mumbai: {
       url: `https://polygon-mumbai.infura.io/v3/${projectId}`,
+      accounts: [key],
     },
   },
   solidity: "0.8.4",
