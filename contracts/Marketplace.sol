d31d806e664296871acb936c1dd72f0d87f46643
Suranjan <suranjanbiswas2002@gmail.com>
2022-02-15 17:32:43 +0530

    add: added constructor initiater for mint and struct_marketplace

--- contracts/Marketplace.sol
+++ contracts/Marketplace.sol
@@ -10,4 +10,21 @@ contract Marketplace is ReentrancyGuard {
     using Counters for Counters.Counter;
     Counters.Counter private _itemIds; // for the items listed on the marketplace
     Counters.Counter private _itemSold;
+
+    address payable owner; // setting as payable as the owner will be paid some value as commission for the sale
+    uint256 listingPrice = 0.025 ether; // as for polygon so in terms of matic
+
+    struct MarketItem {
+        uint256 itemId;
+        address nftContract;
+        uint256 tokenId;
+        address payable seller;
+        address payable owner;
+        uint256 price;
+        uint256 sold;
+    }
+
+    constructor() {
+        owner = payable(msg.sender);
+    }
 }
