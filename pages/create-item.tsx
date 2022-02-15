a261d6bdc59c0bf79c1579d293b17b53fc414e7d
Suranjan <suranjanbiswas2002@gmail.com>
2022-02-16 00:35:57 +0530

    update: updated the texts for the placeholder

--- pages/create-item.tsx
+++ pages/create-item.tsx
@@ -92,21 +92,21 @@ const CreateItem = () => {
     <div className="flex justify-center">
       <div className="w-1/2 flex flex-col pb-12">
         <input
-          placeholder="Asset Name"
+          placeholder="Name"
           className="mt-8 border rounded p-4"
           onChange={(e) =>
             updateFormInput({ ...formInput, name: e.target.value })
           }
         />
         <textarea
-          placeholder="Asset Description"
+          placeholder="Description"
           className="mt-2 border rounded p-4"
           onChange={(e) =>
             updateFormInput({ ...formInput, description: e.target.value })
           }
         />
         <input
-          placeholder="Asset Price in Eth"
+          placeholder="Price in eth"
           className="mt-2 border rounded p-4"
           onChange={(e) =>
             updateFormInput({ ...formInput, price: e.target.value })
