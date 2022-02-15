3351d86fbfa31cd0a3f27732f7ab6a0e6d6e786f
Suranjan <suranjanbiswas2002@gmail.com>
2022-02-15 22:51:05 +0530

    add: added testing for the nft and marketplace

--- test/sample-test.js
+++ test/sample-test.js
@@ -1,18 +1,41 @@
 const { expect } = require("chai");
+const { ethers } = require("hardhat");
 
-describe("Greeter", function () {
-  it("Should return the new greeting once it's changed", async function () {
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
-    await greeter.deployed();
+describe("Marketplace", function () {
+  it("Should create and execute sales on the marketplace", async function () {
+    const [_, buyer] = await ethers.getSigners();
+    const Marketplace = await ethers.getContractFactory("Marketplace");
+    const marketplace = await Marketplace.deploy();
+    await marketplace.deployed();
+    const marketplaceAddress = marketplace.address;
 
-    expect(await greeter.greet()).to.equal("Hello, world!");
+    const NFT = await ethers.getContractFactory("NFT");
+    const nft = await NFT.deploy(marketplaceAddress);
+    await nft.deployed();
+    const nftContractAddress = nft.address;
 
-    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
+    const auctionPrice = ethers.utils.parseUnits("100", "ether");
 
-    // wait until the transaction is mined
-    await setGreetingTx.wait();
+    let listingPrice = await marketplace.getListingPrice();
+    listingPrice = listingPrice.toString();
 
-    expect(await greeter.greet()).to.equal("Hola, mundo!");
+    await nft.createToken("https://www.minions.com");
+    await nft.createToken("https://www.minions1.com");
+
+    await marketplace.createMarketItem(nftContractAddress, 1, auctionPrice, {
+      value: listingPrice,
+    });
+    await marketplace.createMarketItem(nftContractAddress, 2, auctionPrice, {
+      value: listingPrice,
+    });
+
+    await marketplace.connect(buyer).createMarketSale(nftContractAddress, 1, {
+      value: auctionPrice,
+    });
+
+    const items = await marketplace.fetchMarketItems();
+    console.log("Items: ", items);
+
+    expect(items.length).to.be.equal(1);
   });
 });
