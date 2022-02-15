f0409ec161494f214d384a0c1b26c03d11ae0ac0
Suranjan <suranjanbiswas2002@gmail.com>
2022-02-15 23:54:14 +0530

    add: added initial function for loading and buying an nft

--- pages/index.tsx
+++ pages/index.tsx
@@ -75,11 +75,44 @@ export default function Home() {
     return <h1 className="px-20 py-10 text-3xl">No items in marketplace</h1>;
 
   return (
-    <div className={styles.container}>
+    <>
       <Head>
         <title>Marketplace</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-    </div>
+      <div className="flex justify-center">
+        <div className="px-4" style={{ maxWidth: "1600px" }}>
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 pt-4">
+            {nfts.map((nft: any, i) => (
+              <div key={i} className="border shadow rounded-xl overflow-hidden">
+                <img src={nft.image} />
+                <div className="p-4">
+                  <p
+                    style={{ height: "64px" }}
+                    className="text-2xl font-semibold"
+                  >
+                    {nft.name}
+                  </p>
+                  <div style={{ height: "70px", overflow: "hidden" }}>
+                    <p className="text-gray-400">{nft.description}</p>
+                  </div>
+                </div>
+                <div className="p-4 bg-black">
+                  <p className="text-2xl mb-4 font-bold text-white">
+                    {nft.price} ETH
+                  </p>
+                  <button
+                    className="w-full bg-pink-500 text-white font-bold py-2 px-12 rounded"
+                    onClick={() => buyNft(nft)}
+                  >
+                    Buy
+                  </button>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
