dff3c1ea82b57476b21a5a8a034d1216ebfb1779
Suranjan <suranjanbiswas2002@gmail.com>
2022-02-15 15:27:11 +0530

    add: added imports for openzeppelin

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -4,3 +4,4 @@ pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol"; // for setting the token_uri
+import "@openzeppelin/contracts/utils/Counters.sol";
