6ccb9f1dbbb227bd9f8b02d6e0b3701b1289556c
Suranjan <suranjanbiswas2002@gmail.com>
2022-02-15 18:03:35 +0530

    fix: fixed the bugs for marketplace

--- contracts/Marketplace.sol
+++ contracts/Marketplace.sol
@@ -15,24 +15,24 @@ contract Marketplace is ReentrancyGuard {
     uint256 listingPrice = 0.025 ether; // as for polygon so in terms of matic
 
     struct MarketItem {
-        uint itemId;
+        uint256 itemId;
         address nftContract;
         uint256 tokenId;
         address payable seller;
         address payable owner;
         uint256 price;
         bool sold;
-    };
-
-    event MarketItemCreated{
-      uint indexed itemId;
-      address indexed nftContract;
-      uint256 indexed tokenId;
-      address seller;
-      address owner;
-      uint256 price;
-      bool sold;
     }
+
+    event MarketItemCreated(
+        uint256 indexed itemI,
+        address indexed nftContract,
+        uint256 indexed tokenId,
+        address seller,
+        address owner,
+        uint256 price,
+        bool sold
+    );
 
     mapping(uint256 => MarketItem) private _idToMarketItems;
 
@@ -44,25 +44,40 @@ contract Marketplace is ReentrancyGuard {
         return listingPrice;
     }
 
-    function createMarketItem(address nftContract, uint256 tokenId, uint256 price) public payable nonReentrant{
-      require(price > 0, "Price must be at-least 1wei");
-      require(msg.value==listingPrice, "Price must be equal to listing price"); // user must send listing price as payment
+    function createMarketItem(
+        address nftContract,
+        uint256 tokenId,
+        uint256 price
+    ) public payable nonReentrant {
+        require(price > 0, "Price must be at-least 1wei");
+        require(
+            msg.value == listingPrice,
+            "Price must be equal to listing price"
+        ); // user must send listing price as payment
 
-      _itemIds.increment();
-      uint256 itemId = _itemIds.current();
+        _itemIds.increment();
+        uint256 itemId = _itemIds.current();
 
-      idToMarketItems[itemId] = MarketItem({
-        itemId,
-        nftContract,
-        tokenId,
-        payable(msg.sender),
-        payable(address(0)), // owner setting as empty address
-        price,
-        false
-      });
+        _idToMarketItems[itemId] = MarketItem({
+            itemId: itemId,
+            nftContract: nftContract,
+            tokenId: tokenId,
+            seller: payable(msg.sender),
+            owner: payable(address(0)), // owner setting as empty address
+            price: price,
+            sold: false
+        });
 
-      IERC721.transferFrom(msg.sender, address(this), itemId); // transfer ownership of the item to the marketplace
+        IERC721(nftContract).transferFrom(msg.sender, address(this), itemId); // transfer ownership of the item to the marketplace
 
-      emit MarketItemCreated(itemId, nftContract, tokenId, msg.sender, payable(address(0)), price, false);
+        emit MarketItemCreated(
+            itemId,
+            nftContract,
+            tokenId,
+            msg.sender,
+            payable(address(0)),
+            price,
+            false
+        );
     }
 }
