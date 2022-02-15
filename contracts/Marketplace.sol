c12ad69bf0704bafd8062c45d4c32b694fedb000
Suranjan <suranjanbiswas2002@gmail.com>
2022-02-15 17:52:08 +0530

    add: added method for sale creator for item on marketplace

--- contracts/Marketplace.sol
+++ contracts/Marketplace.sol
@@ -43,4 +43,26 @@ contract Marketplace is ReentrancyGuard {
     function getListingPrice() public view returns (uint256) {
         return listingPrice;
     }
+
+    function createMarketItem(address nftContract, uint256 tokenId, uint256 price) public payable nonReentrant{
+      require(price > 0, "Price must be at-least 1wei");
+      require(msg.value==listingPrice, "Price must be equal to listing price"); // user must send listing price as payment
+
+      _itemIds.increment();
+      uint256 itemId = _itemIds.current();
+
+      idToMarketItems[itemId] = MarketItem({
+        itemId,
+        nftContract,
+        tokenId,
+        payable(msg.sender),
+        payable(address(0)), // owner setting as empty address
+        price,
+        false
+      });
+
+      IERC721.transferFrom(msg.sender, address(this), itemId); // transfer ownership of the item to the marketplace
+
+      emit MarketItemCreated(itemId, nftContract, tokenId, msg.sender, payable(address(0)), price, false);
+    }
 }
