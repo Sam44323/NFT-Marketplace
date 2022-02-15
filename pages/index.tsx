122a0863f19602c1feb27fb5701ead0de3c1ac6c
Suranjan <suranjanbiswas2002@gmail.com>
2022-02-15 23:14:45 +0530

    add: added required imports for index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,10 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.scss";
+import { ethers } from "ethers";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import Web3Modal from "web3modal";
 
 export default function Home() {
   return (
