88c7316b1b8009814ee71d86f4641ca196cc1912
Suranjan <suranjanbiswas2002@gmail.com>
2022-02-16 00:35:05 +0530

    add: added the form section for creating a sale

--- pages/create-item.tsx
+++ pages/create-item.tsx
@@ -88,7 +88,41 @@ const CreateItem = () => {
     router.push("/");
   };
 
-  return <></>;
+  return (
+    <div className="flex justify-center">
+      <div className="w-1/2 flex flex-col pb-12">
+        <input
+          placeholder="Asset Name"
+          className="mt-8 border rounded p-4"
+          onChange={(e) =>
+            updateFormInput({ ...formInput, name: e.target.value })
+          }
+        />
+        <textarea
+          placeholder="Asset Description"
+          className="mt-2 border rounded p-4"
+          onChange={(e) =>
+            updateFormInput({ ...formInput, description: e.target.value })
+          }
+        />
+        <input
+          placeholder="Asset Price in Eth"
+          className="mt-2 border rounded p-4"
+          onChange={(e) =>
+            updateFormInput({ ...formInput, price: e.target.value })
+          }
+        />
+        <input type="file" name="Asset" className="my-4" onChange={onChange} />
+        {fileUrl && <img className="rounded mt-4" width="350" src={fileUrl} />}
+        <button
+          onClick={createMarket}
+          className="font-bold mt-4 bg-pink-500 text-white rounded p-4 shadow-lg"
+        >
+          Create Digital Asset
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default CreateItem;
